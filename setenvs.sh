be3bcbc2e7b69cdbb6a38e349c5818cc3cca7aff
mont_si <sergio.montenegro@uni-wuerzburg.de>
2020-01-09 18:05:12 +0100

    Workaround for a strange behavior of bash when calling source inside of another source

--- setenvs.sh
+++ setenvs.sh
@@ -45,4 +45,5 @@ export CPPFLAGS=" -fno-rtti -fno-exceptions  -std=c++14"
 
 #_____________________________________________________ extras, nice to have but not required
 
+export NOT_USED=bash # else the first line in the next script using $_ will deliver a wrong value
 source ${RODOS_HELP_SCRIPTS}/set-git-aware-promt.sh  # nice if you are using git, else remove this line
