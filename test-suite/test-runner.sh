f2519ad2560b706faab48e87b810798fcf96cc54
Daniel Jour <musteresel@gmail.com>
2020-02-12 00:48:31 +0100

    Do comparisions with expected output in test suite
     - Pass path to the txt file with the expected output for each test to
       the test-runner.sh from CMakeLists.txt
    
     - Perform comparisons in test-runner.sh; only use output below
       separation pattern (avoids issues with node id, static
       initialization order, and so on)
    
    Note: The tests now fail because the expected output doesn't match
    some changes done in the previous commits.  There are also some
    outputs which contain build system dependent strings, these need to be
    changed, too.

--- test-suite/test-runner.sh
+++ test-suite/test-runner.sh
@@ -1,8 +1,12 @@
-echo "::: Running $1"
+echo "::: Running $1 against $2"
 # Kill test executable (with SIGKILL) after 6 seconds.  Necessary
 # because sometimes tests might deadlock.
-timeout -s 9 6 $1 | tee "$1.output"
+timeout -s 9 6 $1 > "$1.output"
 # The tests are only used to generate coverage information, thus they
 # are all treated as succeeding.
-exit 0
+linecount=`wc -l "$2"`
+pattern="-----------------------------------------------------"
+grep -h -B 0 -A $linecount -e $pattern $1.output > $1.output_trimmed
+grep -h -B 0 -A $linecount -e $pattern $2 > $1.expected_trimmed
+diff -ruh $1.output_trimmed $1.expected_trimmed
 
