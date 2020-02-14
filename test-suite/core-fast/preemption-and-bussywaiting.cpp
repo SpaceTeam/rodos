489ebdb480ee0035c6544e89b2b56ea53b8c3263
mont_si <sergio.montenegro@uni-wuerzburg.de>
2020-02-14 10:57:04 +0100

    Correst testprogramm preemption-and-bussywaiting.cpp

--- test-suite/core-fast/preemption-and-bussywaiting.cpp
+++ test-suite/core-fast/preemption-and-bussywaiting.cpp
@@ -27,11 +27,11 @@ class HiPriorityThread : public Thread {
 
     void run() {
         volatile int64_t cnt             = 0;
-        int64_t          intervalToPrint = 10;
+        int64_t   intervalToPrint = getSpeedKiloLoopsPerSecond() * 10;
         PRINTF("\nHi prio waits 1 second and then print +\n");
         AT(1 * SECONDS);
         PRINTF("\nHi Priority in busy waiting\n");
-        for (int i=0; i < 1000; i++) {
+        for (int64_t i=0; i < intervalToPrint*20; i++) {
             if(++cnt % intervalToPrint == 0) {
                 PRINTF("+");
                 FFLUSH();
@@ -42,7 +42,7 @@ class HiPriorityThread : public Thread {
         PRINTF("\nHi prio in busy waiting in PRIORITY_FLOORING\n");
         {
             PRIORITY_FLOORING_IN_SCOPE();
-            for (int i=0; i < 1000; i++) {
+            for (int64_t i=0; i < intervalToPrint*20; i++) {
                 if(++cnt % intervalToPrint == 0) {
                     PRINTF("+");
                     FFLUSH();
