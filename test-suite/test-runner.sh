4135b1c920c72cfef4bb45a5b9a3685aa7b019e9
Daniel Jour <musteresel@gmail.com>
2020-02-15 23:26:38 +0100

    Fix linecount in test-runner.sh
     - Linecount is necessary to get all of the required output; previous
       calculation included the filename causing grep to operate on
       multiple files

--- test-suite/test-runner.sh
+++ test-suite/test-runner.sh
@@ -4,7 +4,7 @@ echo "::: Running $1 against $2"
 timeout -s 9 6 $1 > "$1.output"
 # The tests are only used to generate coverage information, thus they
 # are all treated as succeeding.
-linecount=`wc -l "$2"`
+linecount=(`wc -l "$2"`)
 pattern="-----------------------------------------------------"
 grep -h -B 0 -A $linecount -e $pattern $1.output > $1.output_trimmed
 grep -h -B 0 -A $linecount -e $pattern $2 > $1.expected_trimmed
