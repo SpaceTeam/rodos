c7f990c7fd8e2e6d3dd60e0bd430183019f448bc
Daniel Jour <musteresel@gmail.com>
2020-02-16 11:03:29 +0100

    Split tests into run and compare steps
     - One test to run the rodos test application and one test to compare
       its output with the expected output.
    
     - Using CMake fixtures to setup dependencies between the tests.
    
     - Added a only-run-tests dummy test which depends only on the tests
       running the rodos test applications, without the comparisons.

--- test-suite/test-runner.sh
+++ test-suite/test-runner.sh
@@ -1,5 +1,5 @@
 echo "::: Running $1 against $2"
-
+set -e
 # Kill test executable (with SIGKILL) after 8 seconds.  Necessary
 # because sometimes tests might deadlock.
 timeout -s 9 8 $1 > "$1.output"
@@ -7,8 +7,5 @@ timeout -s 9 8 $1 > "$1.output"
 # Remove header / intro from output and expected output
 linecount=`wc -l < "$2"`
 pattern="-----------------------------------------------------"
-grep -h -B 0 -A $linecount -e $pattern $1.output > $1.output_trimmed
-grep -h -B 0 -A $linecount -e $pattern $2 > $1.expected_trimmed
-
-# Compare acutal with expected output
-diff -ruh $1.output_trimmed $1.expected_trimmed
+grep -h -B 0 -A $linecount -e $pattern $1.output > $1.output_trimmed || true
+grep -h -B 0 -A $linecount -e $pattern $2 > $1.expected_trimmed || true
