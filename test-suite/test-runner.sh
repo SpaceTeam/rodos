6010da1c92d495d7478082c5e362256611e6338d
Daniel Jour <musteresel@gmail.com>
2020-02-16 01:04:52 +0100

    More time in test-runner.sh; comments
     - Some tests need (unfortunately) more time, thus extend to 8 seconds
       max allowed time per test.
    
     - Added comments to explain what test-runner.sh is doing.

--- test-suite/test-runner.sh
+++ test-suite/test-runner.sh
@@ -1,12 +1,14 @@
 echo "::: Running $1 against $2"
-# Kill test executable (with SIGKILL) after 6 seconds.  Necessary
+
+# Kill test executable (with SIGKILL) after 8 seconds.  Necessary
 # because sometimes tests might deadlock.
-timeout -s 9 6 $1 > "$1.output"
-# The tests are only used to generate coverage information, thus they
-# are all treated as succeeding.
+timeout -s 9 8 $1 > "$1.output"
+
+# Remove header / intro from output and expected output
 linecount=(`wc -l "$2"`)
 pattern="-----------------------------------------------------"
 grep -h -B 0 -A $linecount -e $pattern $1.output > $1.output_trimmed
 grep -h -B 0 -A $linecount -e $pattern $2 > $1.expected_trimmed
-diff -ruh $1.output_trimmed $1.expected_trimmed
 
+# Compare acutal with expected output
+diff -ruh $1.output_trimmed $1.expected_trimmed
