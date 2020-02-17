1aa6d126ba11ba9f378c4337d9785bf3b2f22601
Flederer <frank.flederer@uni-wuerzburg.de>
2020-02-17 11:47:13 +0100

    cmake: use RODOS_DIR for ports
    This change makes the location of the rodos folder in a project independent.
    Credit to @nicolaskessler for the proposal.

--- cmake/port/sf2.cmake
+++ cmake/port/sf2.cmake
@@ -1,6 +1,8 @@
 set(port_dir "bare-metal/sf2")
 set(is_port_baremetal TRUE)
 
+set(RODOS_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
+
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
@@ -12,21 +14,21 @@ add_compile_options(-Wno-format -fsigned-char -ffunction-sections)
 add_compile_options(-fdata-sections --specs=nano.specs)
 add_compile_options(-mcpu=cortex-m3 -mthumb)
 
-add_link_options(-T${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/CMSIS/startup_gcc/${LINK_SCRIPT})
+add_link_options(-T${RODOS_DIR}/src/bare-metal/sf2/CMSIS/startup_gcc/${LINK_SCRIPT})
 add_link_options(-nostartfiles -nostdlib)
 add_link_options(-fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -g3)
 add_link_options(-fdata-sections -Xlinker --gc-sections -Wl,-Map,sf2.map)
 
 set(sources_to_add
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/rodos/*.cpp
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/rodos/*.c
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/rodos/hal/*.cpp
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/drivers/*.c
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/CMSIS/*.c
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/CMSIS/startup_gcc/*.S
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/drivers_config/sys_config/*.c
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/hal/CortexM3/*.c)
+    ${RODOS_DIR}/src/bare-metal/sf2/rodos/*.cpp
+    ${RODOS_DIR}/src/bare-metal/sf2/rodos/*.c
+    ${RODOS_DIR}/src/bare-metal/sf2/rodos/hal/*.cpp
+    ${RODOS_DIR}/src/bare-metal/sf2/drivers/*.c
+    ${RODOS_DIR}/src/bare-metal/sf2/CMSIS/*.c
+    ${RODOS_DIR}/src/bare-metal/sf2/CMSIS/startup_gcc/*.S
+    ${RODOS_DIR}/src/bare-metal/sf2/drivers_config/sys_config/*.c
+    ${RODOS_DIR}/src/bare-metal/sf2/hal/CortexM3/*.c)
 
 set(directories_to_include
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/rodos
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/sf2/CMSIS)
+    ${RODOS_DIR}/src/bare-metal/sf2/rodos
+    ${RODOS_DIR}/src/bare-metal/sf2/CMSIS)
--- cmake/port/stm32f4.cmake
+++ cmake/port/stm32f4.cmake
@@ -1,6 +1,8 @@
 set(port_dir "bare-metal/stm32f4")
 set(is_port_baremetal TRUE)
 
+set(RODOS_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
+
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
@@ -10,15 +12,15 @@ add_compile_definitions(HSE_VALUE=${OSC_CLK} STM32F40_41xxx USE_STM32_DISCOVERY 
 
 add_compile_options(-nostdlib -gdwarf-2)
 add_compile_options(-mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16)
-add_link_options(-T${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/scripts/stm32_flash.ld)
+add_link_options(-T${RODOS_DIR}/src/bare-metal/stm32f4/scripts/stm32_flash.ld)
 add_link_options(-nostartfiles -nodefaultlibs -nostdlib -Xlinker --gc-sections -fno-unwind-tables -fno-asynchronous-unwind-tables)
 
 set(sources_to_add
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/STM32F4xx_StdPeriph_Driver/src/*.c
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/startup/*.c
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/startup/*.S)
+    ${RODOS_DIR}/src/bare-metal/stm32f4/STM32F4xx_StdPeriph_Driver/src/*.c
+    ${RODOS_DIR}/src/bare-metal/stm32f4/startup/*.c
+    ${RODOS_DIR}/src/bare-metal/stm32f4/startup/*.S)
 
 set(directories_to_include
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/STM32F4xx_StdPeriph_Driver/inc
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/CMSIS/Device/ST/STM32F4xx/Include
-    ${CMAKE_SOURCE_DIR}/src/bare-metal/stm32f4/CMSIS/Include)
+    ${RODOS_DIR}/src/bare-metal/stm32f4/STM32F4xx_StdPeriph_Driver/inc
+    ${RODOS_DIR}/src/bare-metal/stm32f4/CMSIS/Device/ST/STM32F4xx/Include
+    ${RODOS_DIR}/src/bare-metal/stm32f4/CMSIS/Include)
