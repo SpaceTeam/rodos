a2478d1ee5d7f6bedb4b87a9440d33172736fbe6
rodos <sergio.montenegro@uni-wuerzburg.de>
2020-06-05 13:01:31 -0400

    add scripts and program to compute required stack size for threads

--- scripts/help-scrips/rodos-clear.sh
+++ scripts/help-scrips/rodos-clear.sh
@@ -13,7 +13,7 @@ SIZE_B=$(du -sk . | cut -f 1 | head -n 1)
 
 #__________________________________________
 
-CLEAR_LIST=$(find . -type f | grep -E "\.o$|\.out$|sf2.map$|tst$|tst-|tst_|\.elf$|\.bin$|\.hex$|\.img$|temp$|a.out")
+CLEAR_LIST=$(find . -type f | grep -E "\.o$|\.out$|sf2.map$|tst$|tst-|tst_|\.elf$|\.bin$|\.hex$|\.img$|\.ci$|temp$|a.out")
 
 rm -f  ${CLEAR_LIST}
 rm -rf ${RODOS_BUILD}
