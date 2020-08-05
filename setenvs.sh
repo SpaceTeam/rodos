64f1cab3813758043e2d738bc363322d019730f4
Gnarflord <jan@raven.inka.de>
2020-08-05 06:52:53 +0000

    Mad setenv compatible with bash, zsh, korn and -most- POSIX-compliant systems. Previously only worked on bash

--- setenvs.sh
+++ setenvs.sh
@@ -1,8 +1,23 @@
 
 #!/bin/bash
-#_________________________________________Check it has to be sourced!
 scriptName=$_
-if [ $scriptName == $0 ]
+
+#_________________________________________Check it has to be sourced!
+# Source check should work on most shells
+# See this: https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
+sourced=0
+if [ -n "$ZSH_EVAL_CONTEXT" ]; then
+  case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+  [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && sourced=1
+else # All other shells: examine $0 for known shell binary filenames
+  # Detects `sh` and `dash`; add additional shell filenames as needed.
+  case ${0##*/} in sh|dash) sourced=1;; esac
+fi
+
+if [ $sourced -eq 0 ]
 then
    echo "_____________________________________________________ ERROR"
    echo "ERROR: Please call this script as source, and not as subshell"
