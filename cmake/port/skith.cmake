efe6d2ca749d52ead7f5d3f325ced586b4891687
Nicolas KESSLER <nicolaskessler@me.com>
2021-02-25 14:48:45 +0100

    specify 16MHz oscillator

--- cmake/port/skith.cmake
+++ cmake/port/skith.cmake
@@ -1,5 +1,5 @@
 set(board skith)
-set(OSC_CLK 8000000)
+set(OSC_CLK 16000000)
 set(RODOS_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
 set(linker_script ${RODOS_DIR}/src/bare-metal/stm32f4/scripts/stm32_flash.ld)
 
