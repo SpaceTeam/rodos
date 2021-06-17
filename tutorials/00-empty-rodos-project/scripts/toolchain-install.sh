6ac4987cd56c6fd3daaf5a656e84fbd2368eeb25
Nikolaj Bernhardt <bernnico@gmx.de>
2021-06-17 20:22:20 +0000

    Add Cross Compiler tool chain installer script

---
+++ tutorials/00-empty-rodos-project/scripts/toolchain-install.sh
@@ -0,0 +1,25 @@
+!/bin/bash
+
+export TOOLCHAIN_NAME="i686-unknown-linux-gnu"
+export TOOLCHAIN_VERSION="10.2.0"
+
+# Extract the tar archive
+tar xzfv ${TOOLCHAIN_NAME}-${TOOLCHAIN_VERSION}.tar.gz
+
+# Populate directory
+chmod -R u+w /usr/local
+./${TOOLCHAIN_NAME}/bin/${TOOLCHAIN_NAME}-populate -m -s ${TOOLCHAIN_NAME} -d /usr/local/${TOOLCHAIN_NAME} &>/dev/null
+
+# Remove temporary directory & .config file
+rm -r -f ./${TOOLCHAIN_NAME} | true
+rm -f /usr/local/.config | true
+
+rm bin/ar.exe bin/as.exe bin/gcc.exe bin/g++.exe
+
+ln -s /usr/local/i686-unknown-linux-gnu/bin/i686-unknown-linux-gnu-ar.exe /bin/i686-unknown-linux-gnu-ar.exe
+ln -s /usr/local/i686-unknown-linux-gnu/bin/i686-unknown-linux-gnu-as.exe /bin/i686-unknown-linux-gnu-as.exe
+ln -s /usr/local/i686-unknown-linux-gnu/bin/i686-unknown-linux-gnu-gcc.exe /bin/i686-unknown-linux-gnu-gcc.exe
+ln -s /usr/local/i686-unknown-linux-gnu/bin/i686-unknown-linux-gnu-g++.exe /bin/i686-unknown-linux-gnu-g++.exe
+
+echo
+#echo "Links for toolchain were successfully created!"
