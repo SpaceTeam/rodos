1bcc0f870da460cc42d827d19bb86810ad4742d0
Sergio Montenegro <sergio.montenegro@uni-wuerzburg.de>
2022-08-17 06:04:18 +0000

    S3p linkinterfaceuart

--- api/gateway/linkinterfaceuart.h
+++ api/gateway/linkinterfaceuart.h
@@ -1,83 +1,42 @@
-
+/**
+ * @file linkinterfaceuart.h
+ * @author Emilio Miranda
+ * @date created: 16.06.2022
+ * @date modified: 01.07.2022
+ * @brief Implementation of linkinterface and network message with s3p frame
+ * 
+ */
 
 #pragma once
 
 #include "gateway/linkinterface.h"
-#include "gateway/gateway.h"
-#include "gateway/networkmessage.h"
 #include "hal/hal_uart.h"
+#include "s3p-synchronous-interface.h"
 
 
 namespace RODOS {
 
-/**
- * @file linkinterfaceuart.h
- * @date 2012/012/03 10:07
- * @author TW
- *
- *
- * @brief subscribers as gateway
- *
- */
+class LinkinterfaceUART : public Linkinterface, IOEventReceiver, S3pReceiverSynchronous, S3pSenderSynchronous {
+private:
+    const int MAX_UART_MESSAGE_LENGTH = MAX_NETWORK_MESSAGE_LENGTH;
+    
+    HAL_UART *uart;
 
+    uint32_t maxNumOfRetriesPerWrite;  // transmition abort in case of too many errors
+    bool errFLag;               // Tx max attempts reached while sending?
+    volatile bool txInProg;     // True if we are sending a message, otherwise False 
 
-constexpr int MAX_UART_MESSAGE_LENGTH = (MAX_NETWORK_MESSAGE_LENGTH + 6);
-
-constexpr size_t OUTPUT_BUF_SIZE = 64;
-static_assert(OUTPUT_BUF_SIZE > 1, "linkinterface uart: output buffer to small");
-
-class LinkinterfaceUART : public Linkinterface, IOEventReceiver  {
-
-    HAL_UART* uart;
-    //unsigned char inputBuffer[MAX_UART_MESSAGE_LENGTH]; // bom, topicid, message eom
-    uint8_t* inputBuffer;
-
-    /* used to send messages in a block and not char by char
-     * -> important for bluetooth
-     * -> if message is longer than outputBuffer it is split into multiple HAL_UART::write calls
-     */
-    uint8_t outputBuffer[OUTPUT_BUF_SIZE];
-
-    enum ReadStates {
-        WAITING_FOR_START,
-        READING,
-        ESCAPE
-    } readState;
-    int readIndex;
-
-    volatile bool maySend; // controlled by ^s, ^q from reader
-    bool enaTXBreak;  // enable transmit interruption by received character
-
-    NetworkMessage* inMsg;
-    uint8_t currCharFromLastCall;
-    uint8_t prevChar;
-    int inputIndex;
-    bool foundUART_BOM;
-
-    volatile bool transmitinProgrss;
-
-    char getcharWaiting();
-    void putcharEncoded(const bool mark, const uint8_t c);
-    size_t putcharEncodedToBuf(uint8_t* buf, const bool mark, const uint8_t c);
-    bool sendUartBuffer(uint8_t* buf, size_t size);
-    bool processByte(uint8_t input);
-    void myPutChar(uint8_t c);
+    // ___________________________   functions called from s3p encoder and decoder to send and receive
+    void putByte(uint8_t c);
+    uint8_t getByte();
 
 public:
+    LinkinterfaceUART(HAL_UART *uart, uint32_t baudRate, int64_t id = -1, uint32_t maxRetries = 1000);
+    void init(); 
 
-    LinkinterfaceUART(HAL_UART* uart, int64_t id = -1, bool enaTXBreak = true);
-    void init();
-
-    bool isNetworkMsgSent();
     bool sendNetworkMsg(NetworkMessage& outgoingMessage);
     bool getNetworkMsg(NetworkMessage &inMsg, int32_t &numberOfReceivedBytess);
-
-    void onWriteFinished(); // from IOEventReceiver
-
+    bool isNetworkMsgSent();
     virtual void suspendUntilDataReady(int64_t reactivationTime = END_OF_TIME);
 };
-
-
 }  // namespace
-
-
--- src/independent/gateway/linkinterfaceuart.cpp
+++ src/independent/gateway/linkinterfaceuart.cpp
@@ -1,235 +1,104 @@
 
-#include "gateway/linkinterfaceuart.h"
-#include "gateway/networkmessage.h"
-
 /**
  * @file linkinterfaceuart.cc
- * @date 2012/10/26
- * @author Thomas Walter, uni Würzburg
- *
- * Link Interface to uart.
- *
+ * @author Emilio Miranda
+ * @date created: 16.05.2022
+ * @date modified: 01.07.2022
+ * @brief Link Interface to uart.
  *
  */
 
+#include "gateway/linkinterfaceuart.h"
+
 namespace RODOS {
 
-/*********************************************************************************/
-
-static const uint8_t  UART_BOM		=  0x02u;
-static const uint8_t  UART_EOM		=  0x03u;
-static const uint8_t  ENCODED_FF		=  0x7Eu;
-static const uint8_t  STOP_MARK		=  0x18u;
-static const uint8_t  CONTINUE_MARK	=  0x01u;
-static const uint8_t  MARK			=  0xffu;
-
-/*********************************************************************************/
-
-LinkinterfaceUART::LinkinterfaceUART(HAL_UART* uart, int64_t id, bool enaTXBreak) : Linkinterface(id) {
-    this->uart=uart;
-    // this->uart->init();
-    readState = WAITING_FOR_START;
-    readIndex = 0;
-    maySend = true;
-    currCharFromLastCall=0;
-    prevChar=0;
-    inputIndex=0;
-    foundUART_BOM=false;
-    transmitinProgrss=false;
-    this->enaTXBreak = enaTXBreak;
+// __________________________________________ Constructor and Init
+LinkinterfaceUART::LinkinterfaceUART(HAL_UART *uart, uint32_t baudRate, int64_t id, uint32_t maxRetries) : Linkinterface(id) {
+    this->uart = uart;
+    this->uart->init(baudRate);
+    txInProg = false;
+    this->maxNumOfRetriesPerWrite = maxRetries; // when to give up in case of errors
 }
-
-/*********************************************************************************/
-void LinkinterfaceUART::putcharEncoded(const bool mark, const uint8_t c) {
-
-    /** first check if we were stoped by control flow commands **/
-
-    for(int i = 0; i < 5 && !maySend; i++) // ^s-> stop ? but with a limit
-        Thread::suspendCallerUntil(NOW() + 200*MILLISECONDS); // no more than 5 times, (one second)
-
-    maySend = true; // in case the receiver is dead.
-
-    /*** By the way, in linux do not need to send stop/continues...... */
-    /** Not stoped -> send now **/
-    if(mark) {
-        myPutChar(MARK);
-        myPutChar(c);
-        return;						// <------------------------
-    }
-
-    if(c == MARK)  { // the only char for stuffing
-        myPutChar(MARK);
-        myPutChar(ENCODED_FF);
-        return;						// <------------------------
-    }
-    /** nothing special, the char may go out without encoding **/
-    myPutChar(c);
-
-}
-
-void LinkinterfaceUART::myPutChar(uint8_t c) {
-
-        while(uart->putcharNoWait(c) == -1){
-        	uart->suspendUntilWriteFinished();
-        }
-}
-
-
-/*********************************************************************************/
-size_t LinkinterfaceUART::putcharEncodedToBuf(uint8_t* buf, const bool mark, const uint8_t c) {
-
-    if(mark) {
-        *buf++ = MARK;
-        *buf = c;
-        return 2;                     // <------------------------
-    }
-
-    if(c == MARK)  { // the only char for stuffing
-        *buf++ = MARK;
-        *buf = ENCODED_FF;
-        return 2;                     // <------------------------
-    }
-    /** nothing special, the char may go out without encoding **/
-    *buf = c;
-    return 1;
-}
-/*********************************************************************************/
-
-bool LinkinterfaceUART::sendNetworkMsg(NetworkMessage& outgoingMessage)	{
-
-    transmitinProgrss=true;
-    uint32_t lenToSend = static_cast<uint32_t>(outgoingMessage.numberOfBytesToSend());
-    uint8_t* dataToSend = reinterpret_cast<uint8_t*>(&outgoingMessage);
-
-    if (enaTXBreak){ // transmit interruption by received character is enabled
-        putcharEncoded(true, UART_BOM);
-        for(uint32_t i = 0; i < lenToSend; i++) {
-            putcharEncoded(false,dataToSend[i] );
-        }
-        putcharEncoded(true, UART_EOM);
-    }else{ // transmit interruption by received character is disabled -> we can send longer blocks (only important for BT)
-        size_t wrIdx = 0;
-        wrIdx += putcharEncodedToBuf(outputBuffer,true, UART_BOM);
-
-        for(uint32_t i = 0; i < lenToSend; i++) {
-            wrIdx += putcharEncodedToBuf(&outputBuffer[wrIdx],false,dataToSend[i] );
-
-            if(wrIdx >= OUTPUT_BUF_SIZE-2){ // "-2" -> we need some space for UART_EOM
-                if (!sendUartBuffer(outputBuffer,wrIdx)) {
-                    PRINTF("Linkinterface Uart transmit error\n");
-                    return false;
-                }
-                wrIdx = 0;
-            }
-        }
-        wrIdx += putcharEncodedToBuf(&outputBuffer[wrIdx],true, UART_EOM);
-        if (!sendUartBuffer(outputBuffer,wrIdx)) {
-            PRINTF("Linkinterface Uart transmit error\n");
-            return false;
-        }
-    }
-
-    return true;
-}
-
-
-bool LinkinterfaceUART::sendUartBuffer(uint8_t* buf, size_t size){
-    size_t txBytes = 0;
-    size_t retVal = 0;
-    int errCnt = 0;
-    while (txBytes < size){
-        retVal = uart->write(&buf[txBytes],size-txBytes);
-        if (retVal <= 0){
-            errCnt++;
-            if (errCnt > 100){
-                return false;
-            }
-            Thread::suspendCallerUntil(NOW() + 1*MILLISECONDS);
-        }else if ( retVal != size-txBytes ){ // the whole buffer couldn't be sent -> wait a moment and send the rest
-            Thread::suspendCallerUntil(NOW() + 1*MILLISECONDS);
-            txBytes += retVal;
-            errCnt = 0;
-        }else{
-            return true;
-        }
-    }
-
-    return true;
-}
-
-/************************************
- * UART Gateway                     *
- * This methode is for every incoming byte. It collects all recieved character and
- * decodes them to the S3P protocol. Once the EOM is recognized, the data is stored
- * in a NetworkMessage frame and it returns true when the message is complete.
- ************************************/
-
-bool LinkinterfaceUART::processByte(uint8_t inputChar) {
-
-    prevChar = currCharFromLastCall;
-    currCharFromLastCall = inputChar;
-
-    if(inputChar == MARK)	{ return false; } 	// <------ 	fetch next char, ff alone does not help
-
-    if(prevChar == MARK && inputChar == UART_EOM) {	  // end of message detected
-        foundUART_BOM = false;
-        return true;
-    }
-
-    if(prevChar == MARK && inputChar == UART_BOM) {		//Begin of networkmessage detected
-        foundUART_BOM = true;
-        inputIndex = 0;
-        return false;
-    }
-    if(prevChar == MARK && inputChar == STOP_MARK) {
-        maySend = false;
-        return false;
-    }
-    if(prevChar == MARK && inputChar == CONTINUE_MARK) {
-        maySend = true;
-        return false;
-    }
-    if(prevChar == MARK && inputChar == ENCODED_FF) {
-        if(foundUART_BOM) {
-            inputBuffer[inputIndex] = MARK;
-            if(inputIndex < MAX_UART_MESSAGE_LENGTH - 1 ) inputIndex++;
-        }
-        return false;
-    }
-
-    if(prevChar == MARK)  return false; 	// this is an unknown code! just discard
-    if(currCharFromLastCall == MARK) return false; 	// <------ 	fetch next char, ff alone does not help
-
-    inputBuffer[inputIndex] = inputChar;	// place recieved char in inputbuffer
-    if(inputIndex < MAX_UART_MESSAGE_LENGTH - 1) inputIndex++;
-
-    return false;
-}
-
 
 void LinkinterfaceUART::init() { uart->setIoEventReceiver(this); }
 
-bool LinkinterfaceUART::isNetworkMsgSent() { return !transmitinProgrss; }
+//_______________________________________ Sender Side
 
-bool LinkinterfaceUART::getNetworkMsg(NetworkMessage& inMsg,int32_t &numberOfReceivedBytes) {
-    inputBuffer = (unsigned char*)&inMsg;
-    while(uart->isDataReady()) {
-        uint8_t c = static_cast<uint8_t>(uart->getcharNoWait());
-        if(processByte(c)) {
-            numberOfReceivedBytes=inputIndex;
-            return true;
-        }
+/**
+ * @brief send via uart a message with the s3p frame.
+ * 
+ * @param outgoingMessage address of message to tx
+ * @return true if all the bytes of outgoingMessage were successfully tx
+ * @return false if there were more than x failed uart tries (check sendUartBuffer for x)
+ */
+bool LinkinterfaceUART::sendNetworkMsg(NetworkMessage &outgoingMessage)	{
+    txInProg = true;
+    uint8_t* outputBuffer     = reinterpret_cast<uint8_t*>(&outgoingMessage);
+    int      outputBufferSize = static_cast<int>(outgoingMessage.numberOfBytesToSend());
+
+    errFLag = false;
+    sendMsg(outputBufferSize, outputBuffer); // in the S3S lib, encodes and calls putByte
+    
+    txInProg = false; // tx finished!
+    return !errFLag;    
+}
+
+// downcall from S3P encoder
+void LinkinterfaceUART::putByte(uint8_t c) { // caller will be suspended until data is sent
+    
+    uint32_t errCnt = 0;
+    while(uart->write(&c, 1) <= 0) {            
+        errCnt++;
+        if(errCnt >= maxNumOfRetriesPerWrite) {
+            PRINTF("ERROR: linkinterfaceuart.cpp in putByte func - uart transmit error\n");
+            errFLag = true;
+            return; // <<------------------
+        } // max err cnt
+        Thread::suspendCallerUntil(NOW() + 5*MILLISECONDS);
+    } // retry loop
+} // putBytes
+
+
+//____________________________________________ Receiver Side
+
+/**
+ * @brief To receive a message in S3P frame from Uart
+ * 
+ * @param inMsg is a pointer where we store message
+ * @param numberOfReceivedBytes is a pointer where we store the message length
+ * @return true iff a message was receive successfully
+ */
+bool LinkinterfaceUART::getNetworkMsg(NetworkMessage &inMsg, int32_t &numberOfReceivedBytes) {
+    uint8_t* inputBuffer = (unsigned char*)&inMsg;
+
+    numberOfReceivedBytes = getMsg(MAX_UART_MESSAGE_LENGTH, inputBuffer); // in S3P, calls getByte
+
+    return numberOfReceivedBytes != -1;
+}
+
+// dowcall form S3P decoder
+uint8_t LinkinterfaceUART::getByte() { // caller will be suspend until data ready
+
+    while(1) {
+        if(uart->isDataReady()) {
+            return static_cast<uint8_t>(uart->getcharNoWait()); //<<------------------
+	}
+        uart->suspendUntilDataReady(NOW() + 5*MILLISECONDS); 
     }
-    return false;
 }
 
-void LinkinterfaceUART::onWriteFinished() {
-    transmitinProgrss=false;
-    //if(threadToResume) threadToResume->resume();
+
+//_________________________________ Misc Function not used internaly
+
+
+/**
+ * @brief returns true if a message is being sent (in progress)
+ */
+
+bool LinkinterfaceUART::isNetworkMsgSent() { return !txInProg; }
+
+void LinkinterfaceUART::suspendUntilDataReady(int64_t reactivationTime) { // NOT USED??
+        uart->suspendUntilDataReady(reactivationTime);
 }
 
-void LinkinterfaceUART::suspendUntilDataReady(int64_t reactivationTime){
-	uart->suspendUntilDataReady(reactivationTime);
-}
-
-}
+} // namespace
