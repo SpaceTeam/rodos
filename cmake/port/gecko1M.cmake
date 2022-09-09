878c1498795703189403e7f81d176e7070fbe15b
michael.strohmeier@uni-wuerzburg.de <michael.strohmeier@uni-wuerzburg.de>
2022-09-09 16:55:31 +0200

    Fix linker scripts

--- cmake/port/gecko1M.cmake
+++ cmake/port/gecko1M.cmake
@@ -4,6 +4,6 @@ set(SUB_ARCH efr32fg12p)
 set(SUB_ARCH_DIR EFR32FG12P)
 set(SUB_ARCH_FLAGS "EFR32FG12P;EFR32FG12P433F1024GL125;EFR32_SERIES1_CONFIG2_MICRO" )
 set(RAIL_LIB "rail_efr32xg12_gcc_release")
-set(linker_script ${RODOS_DIR}/src/bare-metal/${SUB_ARCH}/scripts/${SUB_ARCH}.ld)
+set(linker_script ${CMAKE_CURRENT_LIST_DIR}/../../src/bare-metal/efr32fg1p/scripts/${SUB_ARCH}.ld)
 
 include(${CMAKE_CURRENT_LIST_DIR}/efr32fg1p.cmake)
