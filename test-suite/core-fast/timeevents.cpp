719f2d656adc9ed19ab221135cfb76a519866508
Minu Föger <foeger@in.tum.de>
2023-03-26 15:41:20 +0200

    fixed TimeEvent related tests

--- test-suite/core-fast/timeevents.cpp
+++ test-suite/core-fast/timeevents.cpp
@@ -9,17 +9,15 @@
 
 #include "rodos.h"
 
-#include "../prt-seconds-now.h"
-
 uint32_t printfMask = 0;    //Will be set to 1 in init of TimeEventTest3 to get the PRINTF then to 0 and the in the handle functions to 1
 
 static Application module01("TestTimeEvents");
 
-class TimeEventTest : public TimeEvent {
+class TimeEventTest1 : public TimeEvent {
   protected:
     void handle() {
         printfMask = 1;
-        xprintf("SINGLE Time Event at %3.9f\n", CNT_SECONDS_NOW());
+        xprintf("[TimeEventTest1::handle] SINGLE TimeEvent at %3.3fs\n", SECONDS_NOW());
     }
 
     void init() {
@@ -27,13 +25,13 @@ class TimeEventTest : public TimeEvent {
     }
 };
 
-static TimeEventTest te01;
+static TimeEventTest1 te01;
 
-class TimeEventTestPeriodic : public TimeEvent {
+class TimeEventTest2 : public TimeEvent {
   public:
     void handle() {
         printfMask = 1;
-        xprintf("Periodic Time Event at %3.9f\n", CNT_SECONDS_NOW());
+        xprintf("[TimeEventTest2::handle] Periodic TimeEvent at %3.3fs\n", SECONDS_NOW());
     }
 
     void init() {
@@ -41,7 +39,7 @@ class TimeEventTestPeriodic : public TimeEvent {
     }
 };
 
-static TimeEventTestPeriodic te02;
+static TimeEventTest2 te02;
 
 class TimeEventTest3 : public TimeEvent {
     int i = 0;
@@ -54,13 +52,12 @@ class TimeEventTest3 : public TimeEvent {
             FFLUSH();
             hwResetAndReboot();
         }
-        xprintf("Activating Time Event at %3.9f, reactivation in 0.02 seconds\n", CNT_SECONDS_NOW());
+        xprintf("[TimeEventTest3::handle] TimeEvent at %3.3f, reactivation in 0.02s\n", SECONDS_NOW());
         activateAt(NOW() + 20 * MILLISECONDS);
     }
 
     void init() {
         printfMask = 1;
-        xprintf("activateAt(NOW() + 12*MILLISECONDS) now=%3.9f\n", CNT_SECONDS_NOW());
         printfMask = 0;
         activateAt(NOW() + 12 * MILLISECONDS);
     }
