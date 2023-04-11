f545a9e3ef40e0481c5dec20438ca364f33d0e50
Minu Föger <foeger@in.tum.de>
2023-04-12 00:19:00 +0200

    fixed preemption unit tests
    * fixed preemption-and-bussywaiting test
    * fixed preemptiontest-and-ceiler test

--- test-suite/core-fast/preemption-and-bussywaiting.cpp
+++ test-suite/core-fast/preemption-and-bussywaiting.cpp
@@ -4,20 +4,23 @@ uint32_t printfMask = 0;
 
 class LowPriorityThread : public StaticThread<> {
   public:
+    static constexpr auto PRINT_INTERVAL = 10 * MILLISECONDS;
+    static constexpr auto WAIT_JITTER    = 2 * MILLISECONDS;
+
     LowPriorityThread() : StaticThread<>("LowPriority", 100) {}
 
     void run() {
         printfMask = 1;
-        volatile int64_t cnt             = 0;
-        int64_t          intervalToPrint = 1000000 * 10;
         PRINTF("\nLow prio begins printing '.'\n");
-        while(1) {
-            cnt = cnt + 1;
-            if(cnt % intervalToPrint == 0) {
-                PRINTF(".");
-                FFLUSH();
-                AT(NOW() + 10 * MILLISECONDS);
-            }
+
+        while(true) {
+            PRINTF(".");
+            FFLUSH();
+
+            Thread::suspendCallerUntil(TimeModel::computeNextBeat(0, PRINT_INTERVAL, NOW()));
+
+            auto nextTick = TimeModel::computeNextBeat(0, PRINT_INTERVAL, NOW());
+            while(NOW() < nextTick) {}
         }
     }
 } lowPrio;
@@ -26,36 +29,76 @@ class LowPriorityThread : public StaticThread<> {
 
 class HiPriorityThread : public StaticThread<> {
   public:
+    static constexpr auto PRINT_INTERVAL = 10 * MILLISECONDS;
+
+    /*
+     * Wait period before printing is necessary to avoid deadlock of printf
+     * -> if suspend of LowPriorityThread interrupts PRINTF of HiPriorityThread => deadlock
+     * => solution: shift HiPriorityThread period a bit so that periods don't overlap
+     */
+    static constexpr auto PRINTF_WAIT_TIME = 4 * MILLISECONDS;
+    static constexpr auto WAIT_JITTER      = 2 * MILLISECONDS;
+
+    static constexpr auto INIT_WAIT_TIME   = 100 * MILLISECONDS;
+    static constexpr auto BASE_PRINTF_BEAT = INIT_WAIT_TIME + PRINTF_WAIT_TIME;
+
+    static constexpr auto START_LOOP_1    = INIT_WAIT_TIME;
+    static constexpr auto DURATION_LOOP_1 = 200 * MILLISECONDS;
+
+    static constexpr auto START_LOOP_2    = START_LOOP_1 + DURATION_LOOP_1;
+    static constexpr auto DURATION_LOOP_2 = 400 * MILLISECONDS;
+
     HiPriorityThread() : StaticThread<>("HiPriority", 200) {}
 
     void run() {
         printfMask = 1;
-        volatile int64_t cnt             = 0;
-        int64_t   intervalToPrint = getSpeedKiloLoopsPerSecond() * 10;
-        PRINTF("\nHi prio waits 1 second and then print +\n");
-        AT(1 * SECONDS);
-        PRINTF("\nHi Priority in busy waiting\n");
-        for (int64_t i=0; i < intervalToPrint*20; i++) {
-            cnt = cnt + 1;
-            if(cnt % intervalToPrint == 0) {
-                PRINTF("+");
+
+        PRINTF("\nHi prio waits and then prints '+'\n");
+        suspendCallerUntil(INIT_WAIT_TIME + WAIT_JITTER);
+
+        PRINTF("\nHi prio in busy waiting\n");
+        for(auto nextTick = START_LOOP_1 + PRINTF_WAIT_TIME; true;) {
+            if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
+                xprintf("+");
                 FFLUSH();
             }
+            if(NOW() >= START_LOOP_1 + DURATION_LOOP_1) {
+                break;
+            }
+            nextTick = TimeModel::computeNextBeat(BASE_PRINTF_BEAT, PRINT_INTERVAL, NOW());
+            busyWaitUntil(nextTick);
         }
 
-
+        /*
+         * Hi prio in busy waiting in PRIORITY_FLOORING
+         * (time diagram sketch, HiPriorityThread period shifted a bit):
+         *
+         * LowPriorityThread: L---xxxxL---xxxxL---xxxxL---
+         * HiPriorityThread:  -HxxxHxxxHxxxHxxxHxxxHxxxHxxx
+         * together:          LHxxxxxxLHxxxxxxLHxxxxxxLHxxx
+         *
+         * -> 'L/H': print character
+         * -> 'x': busy waiting loop
+         * -> '-': suspending
+         */
         PRINTF("\nHi prio in busy waiting in PRIORITY_FLOORING\n");
         {
             PRIORITY_FLOORING_IN_SCOPE();
-            for (int64_t i=0; i < intervalToPrint*20; i++) {
-                cnt = cnt + 1;
-                if(cnt % intervalToPrint == 0) {
-                    PRINTF("+");
+            yield();
+
+            for(auto nextTick = START_LOOP_2 + PRINTF_WAIT_TIME; true;) {
+                if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
+                    xprintf("+");
                     FFLUSH();
                 }
+                if(NOW() >= START_LOOP_2 + DURATION_LOOP_2) {
+                    break;
+                }
+                nextTick = TimeModel::computeNextBeat(BASE_PRINTF_BEAT, PRINT_INTERVAL, NOW());
+                busyWaitUntil(nextTick);
             }
         }
-        PRINTF("\nHi prio Ends\n");
+        PRINTF("\nHi prio ends\n");
         hwResetAndReboot();
     }
 } hiPri;
--- test-suite/core-slow/preemptiontest-and-ceiler.cpp
+++ test-suite/core-slow/preemptiontest-and-ceiler.cpp
@@ -2,27 +2,27 @@
 
 uint32_t printfMask = 0;
 
-/****** Use modulId 2000 just be be found by other example: ceiler **/
-
-static Application module01("PreemptiveTest", 2000);
+/****** Use moduleId 2000 just be be found by other example: ceiler **/
+static Application module01("PreemptionTest", 2000);
 
 class HighPriorityThread : public StaticThread<> {
   public:
+    static constexpr auto PRINT_INTERVAL = 500 * MILLISECONDS;
+
     HighPriorityThread() : StaticThread<>("HiPriority", 25) {
     }
 
     void init() {
         printfMask = 1;
-        xprintf(" hipri = '*'\n");
+        xprintf(" hipri  = '*'\n");
         printfMask = 0;
     }
 
     void run() {
         printfMask = 1;
-        while(1) {
+        TIME_LOOP(0, PRINT_INTERVAL) {
             xprintf("*");
             FFLUSH();
-            suspendCallerUntil(NOW() + 500 * MILLISECONDS);
         }
     }
 };
@@ -30,34 +30,33 @@ class HighPriorityThread : public StaticThread<> {
 
 class LowPriorityThread : public StaticThread<> {
   public:
+    static constexpr auto PRINT_INTERVAL   = 50 * MILLISECONDS;
+    static constexpr auto PRINTF_WAIT_TIME = 5 * MILLISECONDS;
+    static constexpr auto WAIT_JITTER      = 2 * MILLISECONDS;
+
     LowPriorityThread() : StaticThread<>("LowPriority", 10) {
     }
 
     void init() {
         printfMask = 1;
-        xprintf(" lopri = '.'\n");
+        xprintf(" lopri  = '.'\n");
         printfMask = 0;
     }
 
     void run() {
         printfMask = 1;
-        volatile int64_t cnt             = 0;
-        int64_t          intervalToPrint = getSpeedKiloLoopsPerSecond() * 10;
-        while(1) {
-            cnt = cnt + 1;
-            if(cnt % intervalToPrint == 0) {
+
+        for(auto nextTick = PRINTF_WAIT_TIME; true;) {
+            if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
                 xprintf(".");
                 FFLUSH();
             }
+            nextTick = TimeModel::computeNextBeat(PRINTF_WAIT_TIME, PRINT_INTERVAL, NOW());
+            busyWaitUntil(nextTick);
         }
     }
 };
 
-
-/******************/
-
-HighPriorityThread highPriorityThread;
-LowPriorityThread  lowPriorityThread;
 
 /******************/
 
@@ -65,70 +64,116 @@ static Application module02("PriorityCeiling", 3000);
 
 class PriorityCeiler : public StaticThread<> {
   public:
-    PriorityCeiler() : StaticThread<>("PriotyCeiler", 10) {
+    static constexpr auto PRINT_INTERVAL   = 50 * MILLISECONDS;
+    static constexpr auto PRINTF_WAIT_TIME = 10 * MILLISECONDS;
+    static constexpr auto WAIT_JITTER      = 2 * MILLISECONDS;
+
+    static constexpr auto INIT_SUSPEND_TIME = 3 * SECONDS;
+    static constexpr auto BASE_PRINTF_BEAT  = INIT_SUSPEND_TIME + PRINTF_WAIT_TIME;
+
+    static constexpr auto START_LOOP_1          = INIT_SUSPEND_TIME;
+    static constexpr auto DURATION_LOOP_1       = 5 * SECONDS;
+    static constexpr auto START_LOOP_2          = START_LOOP_1 + DURATION_LOOP_1;
+    static constexpr auto DURATION_LOOP_2       = 3 * SECONDS;
+    static constexpr auto START_LOOP_3          = START_LOOP_2 + DURATION_LOOP_2;
+    static constexpr auto DURATION_LOOP_3       = 5 * SECONDS;
+    static constexpr auto START_LOOP_4          = START_LOOP_3 + DURATION_LOOP_3;
+    static constexpr auto DURATION_LOOP_4       = 3 * SECONDS;
+    static constexpr auto START_LAST_SUSPEND    = START_LOOP_4 + DURATION_LOOP_4;
+    static constexpr auto DURATION_LAST_SUSPEND = 2 * SECONDS;
+
+    PriorityCeiler() : StaticThread<>("PriorityCeiler", 10) {
     }
 
     void init() {
         printfMask = 1;
-        xprintf(" ceiler = '+'\n");
+        xprintf(" ceiler = '-'\n");
         printfMask = 0;
     }
 
 
     void run() {
         printfMask = 1;
-        suspendCallerUntil(3 * SECONDS);
-        int64_t aproxLoopsFor2Seconds = getSpeedKiloLoopsPerSecond() * 1000LL;
-        int64_t aproxLoopsForPrint    = aproxLoopsFor2Seconds / 80;
 
-        xprintf("\nPriority ceiler 122 times normal\n");
-        for(int64_t cnt = 0; cnt < aproxLoopsFor2Seconds; cnt++) {
-            if(cnt % aproxLoopsForPrint == 0) {
+        suspendCallerUntil(INIT_SUSPEND_TIME + WAIT_JITTER);
+
+        xprintf("\nPriorityCeiler: %llds normal\n", DURATION_LOOP_1 / SECONDS);
+        for(auto nextTick = START_LOOP_1 + PRINTF_WAIT_TIME; true;) {
+            if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
                 xprintf("-");
                 FFLUSH();
             }
+            if(NOW() >= START_LOOP_1 + DURATION_LOOP_1) {
+                break;
+            }
+            nextTick = TimeModel::computeNextBeat(BASE_PRINTF_BEAT, PRINT_INTERVAL, NOW());
+            busyWaitUntil(nextTick);
         }
 
-        xprintf("\nPriority ceiler 122 tmes prio ceiling\n");
+        xprintf("\nPriorityCeiler: %llds ceiling\n", DURATION_LOOP_2 / SECONDS);
         {
             PRIORITY_CEILER_IN_SCOPE();
-            for(int64_t cnt = 0; cnt < aproxLoopsFor2Seconds; cnt++) {
-                if(cnt % aproxLoopsForPrint == 0) {
+
+            for(auto nextTick = START_LOOP_2 + PRINTF_WAIT_TIME; true;) {
+                if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
                     xprintf("-");
                     FFLUSH();
                 }
+                if(NOW() >= START_LOOP_2 + DURATION_LOOP_2) {
+                    break;
+                }
+                nextTick = TimeModel::computeNextBeat(BASE_PRINTF_BEAT, PRINT_INTERVAL, NOW());
+                busyWaitUntil(nextTick);
             }
         }
 
-        xprintf("\nPriority ceiler 122 times normal\n");
-        for(int64_t cnt = 0; cnt < aproxLoopsFor2Seconds; cnt++) {
-            if(cnt % aproxLoopsForPrint == 0) {
+        xprintf("\nPriorityCeiler: %llds normal\n", DURATION_LOOP_3 / SECONDS);
+        for(auto nextTick = START_LOOP_3 + PRINTF_WAIT_TIME; true;) {
+            if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
                 xprintf("-");
                 FFLUSH();
             }
+            if(NOW() >= START_LOOP_3 + DURATION_LOOP_3) {
+                break;
+            }
+            nextTick = TimeModel::computeNextBeat(BASE_PRINTF_BEAT, PRINT_INTERVAL, NOW());
+            busyWaitUntil(nextTick);
         }
 
-
-        xprintf("\nPriority ceiler 122 times normal\n");
-        for(int64_t cnt = 0; cnt < aproxLoopsFor2Seconds; cnt++) {
-            if(cnt % aproxLoopsForPrint == 0) {
+        xprintf("\nPriorityCeiler: %llds normal + suspending for %llds\n",
+                DURATION_LOOP_4 / SECONDS,
+                DURATION_LAST_SUSPEND / SECONDS);
+        for(auto nextTick = START_LOOP_4 + PRINTF_WAIT_TIME; true;) {
+            if(NOW() >= nextTick && NOW() <= nextTick + WAIT_JITTER) {
                 xprintf("-");
                 FFLUSH();
             }
+            if(NOW() >= START_LOOP_4 + DURATION_LOOP_4) {
+                break;
+            }
+            nextTick = TimeModel::computeNextBeat(BASE_PRINTF_BEAT, PRINT_INTERVAL, NOW());
+            busyWaitUntil(nextTick);
         }
 
+        suspendCallerUntil(START_LAST_SUSPEND + DURATION_LAST_SUSPEND);
 
-        AT(NOW()+ 2*SECONDS);
-        xprintf("\n Test terminaes\n");
-        hwResetAndReboot ();
-        
+        xprintf("\nTest terminates\n");
+        hwResetAndReboot();
     }
 };
 
 
 /******************/
 
-PriorityCeiler priorityCeiler;
+/**
+ * @name Order of instantiation matters
+ * (e.g. PriorityCeiler must be scheduled before LowPriorityThread initially)
+ * @{
+ */
+HighPriorityThread highPriorityThread;
+LowPriorityThread  lowPriorityThread;
+PriorityCeiler     priorityCeiler;
+/** @} */
 
 /******************/
 
@@ -139,8 +184,6 @@ void MAIN() {
         xprintf("To be able to test it compile together with PreemptionTest\n");
         xprintf("**********************\n\n\n");
     } else {
-        xprintf("Preemptiontest found\n");
+        xprintf("PreemptionTest found\n");
     }
 }
-
-
