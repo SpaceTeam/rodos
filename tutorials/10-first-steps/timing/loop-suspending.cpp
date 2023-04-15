0cce342235318971adbed4fe8e197fd5204e6dab
Minu Föger <foeger@in.tum.de>
2023-04-15 20:16:55 +0200

    removed false schedulerRunning assignment in linux-x86 port

--- tutorials/10-first-steps/timing/loop-suspending.cpp
+++ tutorials/10-first-steps/timing/loop-suspending.cpp
@@ -7,24 +7,6 @@ namespace RODOS {
 
 constexpr auto INTERVAL = 1250 * MILLISECONDS;
 
-class DummyThread : public StaticThread<> {
-  public:
-    DummyThread(int32_t prio, int64_t start)
-      : StaticThread<>("dummyThread", prio), m_start(start) {}
-
-    void run() {
-        TIME_LOOP(m_start, INTERVAL) {
-            busyWaitUntil(TimeModel::computeNextBeat(0, INTERVAL / 2, NOW()));
-        }
-    }
-
-  private:
-    int64_t m_start;
-};
-
-DummyThread dummyThread1{ 100, 0 * MILLISECONDS };
-DummyThread dummyThread2{ 200, 200 * MILLISECONDS };
-
 class PeriodicOutputThread : public StaticThread<> {
   protected:
     void run() {
