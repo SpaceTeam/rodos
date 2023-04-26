6e82b3126ce3e9639b9a69b61cd8ae664ffa200a
Michael Zehrer <michael.zehrer@stud-mail.uni-wuerzburg.de>
2023-04-26 16:54:32 +0200

    fix the barrier test ("core-slow/barrier-test.cpp")
    - change the timeout from 8 seconds to 25
    - fix the formatting and some typos

--- test-suite/core-slow/barrier-test.cpp
+++ test-suite/core-slow/barrier-test.cpp
@@ -9,37 +9,34 @@ Barrier barrier;
 class Waiter : public StaticThread<> {
     void run() {
         printfMask = 1;
-        while(1) {
+        while (true) {
             PRINTF("before barrier\n");
-            bool ok = barrier.waitForSignal(true, NOW() + 2  * SECONDS);
+            bool ok = barrier.waitForSignal(true, NOW() + 2 * SECONDS);
             PRINTF("  waitFor = %s\n", ok ? "ok" : "Timeout");
         }
     }
 
   public:
     Waiter() : StaticThread<>() {}
-} watingTrehad;
+} waitingThread;
 
 
 class SignalingThread : public StaticThread<> {
     void run() {
         TIME_LOOP(1 * SECONDS, 500 * MILLISECONDS) {
-           int action = uint32Rand() % 4;
-	   if(action == 0) {
-               PRINTF("                            Unblocking\n");
-               bool ok = barrier.unblock();
-	       PRINTF("  unblock  = %d\n", ok);
-	   } else {
-               PRINTF("                            no action\n");
-	   }
+            int action = uint32Rand() % 4;
+            if (action == 0) {
+                PRINTF("                            Unblocking\n");
+                bool ok = barrier.unblock();
+                PRINTF("  unblock  = %d\n", ok);
+            } else {
+                PRINTF("                            no action\n");
+            }
 
-           if(NOW() > 10*SECONDS) hwResetAndReboot();
-            
+            if (NOW() > 10 * SECONDS) hwResetAndReboot();
         }
     }
 
   public:
     SignalingThread() : StaticThread<>() {}
 } signalingThread;
-
-
