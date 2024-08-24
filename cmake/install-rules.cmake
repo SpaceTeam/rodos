c55e692543a2d8fb971f2f738082382bb5d997e7
Patrick Kappl <patrick.kappl@spaceteam.at>
2024-08-25 01:44:35 +0200

    Make `rodos_rodos` a clone instead of linking to `rodos_without_main`
    The `target_link_libraries()` approach did not work because the linker
    always complained that `main()` is not defined in
    `librodos_without_main.a`.

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -56,6 +56,7 @@ endif ()
 # I add all of them and use this foreach loop cause I don't know how to do this the install(TARGET
 # ... INCLUDES DESTINATION ...) way.
 foreach(dir ${directories_to_include})
+    target_include_directories(rodos_rodos PUBLIC $<INSTALL_INTERFACE:${rodos_INSTALL_INCLUDEDIR}/${dir}>)
     target_include_directories(rodos_without_main PUBLIC $<INSTALL_INTERFACE:${rodos_INSTALL_INCLUDEDIR}/${dir}>)
 endforeach()
 
@@ -64,7 +65,7 @@ set(rodos_INSTALL_LIBDIR ${CMAKE_INSTALL_LIBDIR}/${package})
 # Define how to install which targets and create an export. If possible define the include
 # directories for the exported targets here.
 install(
-    TARGETS rodos_without_main rodos_rodos EXPORT rodosTargets
+    TARGETS rodos_rodos rodos_without_main EXPORT rodosTargets
     ARCHIVE
         DESTINATION ${rodos_INSTALL_LIBDIR}
     INCLUDES
